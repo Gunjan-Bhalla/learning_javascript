c64c71b308c7687e387f343f7765f6ebb612cea6
Gunjan <87493284+Gunjan-Bhalla@users.noreply.github.com>
2024-02-27 16:26:30 +0000

    datatypes

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,25 @@
+"use strict"; //treat all JS code as newer version
+
+// alert("Hello Mr. Nobita") //We are using nodeJs, not browser
+
+console.log(3 
+    + 
+    3) //code readability should be high
+
+
+    let name = "Doraemon"
+    let age = 19
+    let isLoggedIn = false
+
+    //number => 2 to power 53
+    //bigInt
+    //string => " "
+    //boolean => true/false
+    //null =>standalone value ==> basically isme abhi value assign nahi hui hai
+    //undefined => //abhi value assign nahi hui hai
+    //symbol => unique
+
+    //object
+
+    console.log(typeof undefined);  //output => undefined
+    console.log(typeof null); //output => object
