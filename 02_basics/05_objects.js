7b1ed2549fe9cb43b036aedce3b39a879a9e1186
Gunjan <87493284+Gunjan-Bhalla@users.noreply.github.com>
2024-03-09 11:38:16 +0000

    json

---
+++ 02_basics/05_objects.js
@@ -0,0 +1,21 @@
+ const course = {
+    coursename : "javascript practise",
+    price: "999",
+    courseInstructor: "Doraemon"
+ }
+
+ const {courseInstructor : instructor} = course
+ //console.log(courseInstructor);
+ console.log(instructor); 
+ 
+//  {
+//     "name": "doraemon",
+//     "coursename": "js",
+//     "price": "free"
+//  }
+
+ [
+    {},
+    {},
+    {}
+ ]
