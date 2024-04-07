77cb10d2f69a6cc6ec85b71a0695b8891d354b3c
Gunjan <87493284+Gunjan-Bhalla@users.noreply.github.com>
2024-04-07 09:49:03 +0000

    loops

---
+++ 05_iterations/02_while_doWhile.js
@@ -0,0 +1,23 @@
+// while and do while loop
+
+let index = 0
+while(index <= 10){
+    console.log(`Vlaue of index is ${index}`);
+    index = index + 2;
+}
+
+let myArray = ['flash', 'superman','batman']
+let arr = 0;
+while(arr < myArray.length){
+    console.log(`Vlaue is ${myArray[arr]}`);
+    arr++;
+}
+
+
+// ==========do while=====================
+
+let score = 1;
+do{
+    console.log(`Score is ${score}`);
+    score++;
+} while(score <= 10);
